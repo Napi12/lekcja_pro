0f8fb6e2a0e9d583387b4b4415ba495f8007b9a3
Napi12 <145258570+Napi12@users.noreply.github.com>
2023-09-17 12:04:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+meme_dict ={
+    "LOL" : "odpowiedź na coś zabawnego",
+    "CRINGE" : "coś dziwnego lub wstydliwego",
+    "ROFL" : "odpowiedź na żart",
+    "SHEESH" : "lekka dezaprobata",
+    "CREEPY" : "straszny, złowieszczy",
+    "AGGRO" : "stać się agresywnym/zły",
+}
 
+name = input("wpisz słowo urzywaj wielkich liter")
+if word in meme_dict.keys():
+    print (word, ":" ,meme_dict[name])
+    
+else:
+    print("nie znam takiego kontaktu")
